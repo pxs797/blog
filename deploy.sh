cca78f4b20349a96b10f39ebfda9826172387d10
mark <1210917553@qq.com>
2022-05-18 00:52:23 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd docs
+# cd docs
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:pxs797/pxs797.github.io.git main
+# git push -f git@github.com:pxs797/pxs797.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:pxs797/blog.git main
 
 cd -
