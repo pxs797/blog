0ca1d76580235f3d97637cc466f9a2b28be70362
mark <1210917553@qq.com>
2022-05-17 23:51:09 +0800

    add

---
+++ .vuepress/config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  "title": "破晓blog",
+  "description": "前端学习记录",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "GitHub",
+        "link": "https://github.com/pxs797",
+        "icon": "reco-github"
+      }
+    ],
+    "sidebar": {
+
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Mark Peng",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2022"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroText: 破晓blog
+tagline: 记录前端学习
+# heroImage: /hero.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
