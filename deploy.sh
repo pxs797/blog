1a156f91cb9a71f9cee2abc71822e5a98bdcf9ff
mark <1210917553@qq.com>
2022-05-18 20:36:11 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:pxs797/pxs797.github.io.git master
+# git push -f git@github.com:pxs797/pxs797.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:pxs797/blog.git main
+git push -f git@github.com:pxs797/blog.git master
 
 cd -
